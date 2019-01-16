44b54a96600d95584e15dbfe7f5853bd372de6c5
rockyMountainRobotics <programmingrmr662@gmail.com>
2019-01-15 18:18:08 -0700

    Commented and Cleaned up Code :D

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,25 +7,8 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.cameraserver.CameraServer;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.IterativeRobot;
 
 
 /**
@@ -33,55 +16,24 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  
-  private WPI_TalonSRX leftFront;
-  private WPI_TalonSRX rightFront;
-  private WPI_TalonSRX leftBack;
-  private WPI_TalonSRX rightBack;
-  
 
-  private SpeedControllerGroup leftDrive;
-  private SpeedControllerGroup rightDrive;
-  
-  XboxController driveController;
-
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-
+  //create a component array 
   Component[] parts = new Component[10];
+
+
 
   @Override
   public void robotInit() {
+
+    //Initialize a Drive Component
     parts[0] = new Drive();
 
-    /*
-    leftFront = new WPI_TalonSRX(7);
-    rightFront = new WPI_TalonSRX(6);
-    leftBack = new WPI_TalonSRX(8);
-    rightBack = new WPI_TalonSRX(5);
-
-    leftFront.setInverted(true);
-    //leftBack.setInverted(true);
-    //rightFront.setInverted(true);
-    
-    leftDrive = new SpeedControllerGroup(leftFront, leftBack);
-    rightDrive = new SpeedControllerGroup(rightFront, rightBack);
-
-
-    m_myRobot = new DifferentialDrive(leftDrive, rightDrive);
-
-    driveController = new XboxController(0);
-    //m_leftStick = new Joystick(0);
-    //m_rightStick = new Joystick(1);
-
-    */
   }
 
   @Override
   public void teleopPeriodic() {
+
+    //Update the component
     parts[0].update();
-    //m_myRobot.arcadeDrive(.6 *(-driveController.getY(Hand.kLeft)), .6*(driveController.getX(Hand.kLeft)));
-    //m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
   }
 }
