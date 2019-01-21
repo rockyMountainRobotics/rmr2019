339109580f7dd6c97aea9fe05f6d1db39bc25ad3
rockyMountainRobotics <programmingrmr662@gmail.com>
2019-01-21 14:17:55 -0700

    needs encoder added

--- src/main/java/frc/robot/BallManip.java
+++ src/main/java/frc/robot/BallManip.java
@@ -1,3 +1,5 @@
+package frc.robot;
+
 import edu.wpi.first.wpilibj.XboxController;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
@@ -15,11 +17,14 @@ public class BallManip implements Component
     final static double DEADZONE_2;
     
     //Multiplier
-    final double MULTIPLIER = .9;
+    final double MULTIPLIER;
 
     //Motors (left side and right side of ball manipulator) CANTalon or SRX?????
-    private CANTalon leftSide;
-    private CANTalon rightSide;
+    private CANTalon leftSuck;
+    private CANTalon rightSuck;
+    private CANTalon armMover;
+
+    private double speed;
 
     
 
@@ -28,52 +33,86 @@ public class BallManip implements Component
 
     public BallManip()
     {
-        leftSide = new CANTalon(RobotMap.TOP_LEFT_MOTOR);
-        rightSide = new CANTalon(RobotMap.TOP_RIGHT_MOTOR);
+        leftSuck = new CANTalon(RobotMap.TOP_LEFT_MOTOR);
+        rightSuck = new CANTalon(RobotMap.TOP_RIGHT_MOTOR);
+        armMover = new CANTalon(RobotMap.TOP_CENTER_MOTOR);
+        speed = 0.0;
 
+        MULTIPLIER = .9;
         DEADZONE_1 = -.5;
         DEADZONE_2 = .5;
 
         //INVERTS the RIGHT side motor so that both sides spin inwards. Might need to change this if
         //A) inverting doesn't do what I thought it did or B) the LEFT side needs to be inverted.
-        rightSide.setInverted(true);
+        rightSuck.setInverted(true);
 
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void update()
     {
-        //If the limit switch is not pressed (no ball there)
-        if(!limitSwitchBack.get())
+        //Stuff only works when in ball mode
+        if(SwitchMode.mode.equals("C"))
         {
             //When left trigger is pressed and right trigger isn't being pressed
-            if(RobotMap.manipController.getRawButton(XboxMap.L_ANALOG) == true && RobotMap.manipController.getRawButton(XboxMap.R_ANALOG) != true)
+            if(RobotMap.manipController.getRawButton(XboxMap.L_ANALOG) == true || RobotMap.manipController.getRawButton(XboxMap.R_ANALOG) == true)
             {
-                leftSide.set(1 * MULTIPLIER);
-                rightSide.set(1 * MULTIPLIER);
+                ballSuck();
+            }
+            
+            //When they push the joystick
+            if(Math.abs(RobotMap.manipController.getRawAxis(XboxMap.RIGHT_JOY_VERT)) > DEADZONE_1)
+            {
+                armMover.set(RobotMap.manipController.getRawAxis(XboxMap.RIGHT_JOY_VERT));
             }
         }
-
-        //When right trigger is pressed and left trigger isn't being pressed
-        if(RobotMap.manipController.getRawButton(XboxMap.R_ANALOG) == true && RobotMap.manipController.getRawButton(XboxMap.L_ANALOG) != true)
-        {
-            //is this how it works? who knows : )
-            leftSide.set(-1 * MULTIPLIER);
-            rightSIde.set(-1 * MULTIPLIER);
-        }
-        
-        if(RobotMap.manipController.getRawAxis(XboxMap.RIGHT_JOY_VERT))
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void autoUpdate()
     {
         //Will probably need to call update()
 
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void disable()
     {
-        leftSide.set(0);
-        rightSide.set(0);
+        leftSuck.set(0);
+        rightSuck.set(0);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void ballSuck()
+    {
+        //If there is a ball and its trying to SUCC, stops it from burning a hole in the ball. (limitSwitchBack.get() may need to be inversed)
+        if(limitSwitchBack.get() && Math.abs(leftSuck.get()) > 0 || Math.abs(rightSuck.get() < 0) )
+        {
+            leftSuck.set(0);
+            rightSuck.set(0);
+        }
+
+        //When left trigger is pressed and right trigger isn't being pressed
+        if(RobotMap.manipController.getRawButton(XboxMap.L_ANALOG) == true && RobotMap.manipController.getRawButton(XboxMap.R_ANALOG) != true)
+         {
+             leftSuck.set(1 * MULTIPLIER);
+             rightSuck.set(1 * MULTIPLIER);
+         }
+
+        //When right trigger is pressed and left trigger isn't being pressed
+        if(RobotMap.manipController.getRawButton(XboxMap.R_ANALOG) == true && RobotMap.manipController.getRawButton(XboxMap.L_ANALOG) != true)
+        {
+            //is this how it works? who knows : )
+            leftSuck.set(-1 * MULTIPLIER);
+            rightSuck.set(-1 * MULTIPLIER);
+        }
+    }
+
+    public void reset()
+    {
+        //Need to fix for encoder, so that the motors don't just grind the arm into the robot.
+        armMover.set(-.5);
+        
     }
 
 }
