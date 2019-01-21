61d08c3ee0d94e1a57b2412c2bb78328696c1bea
rockyMountainRobotics <programmingrmr662@gmail.com>
2019-01-21 14:38:01 -0700

    Update Robot Map.java
    Includes motors for all manipulator components (I hope), PORTS NOT CORRECT

--- src/main/java/frc/robot/Robot Map.java
+++ src/main/java/frc/robot/Robot Map.java
@@ -10,7 +10,18 @@ public class RobotMap
 	public final static int LEFT_FRONT_MOTOR = 7;
 	public final static int RIGHT_BACK_MOTOR = 5;
     public final static int LEFT_BACK_MOTOR = 8;
-    
+	
+	public final static int TOP_LEFT_MOTOR = 1;
+	public final static int TOP_RIGHT_MOTOR = 1;
+	public final static int TOP_CENTER_MOTOR = 1;
+	public final static int ELEVATOR = 0;
+	
+	public final static int BEAK_SOLENOID = 0;
+	public final static int TRACK_SOLENOID = 1;
+	
+	public final static int LIMIT_TOP = 0;
+	public final static int LIMIT_BOTTOM = 0;
+	
     public static XboxController driveController = new XboxController(0);
     public static XboxController manipController = new XboxController(1);
 }
