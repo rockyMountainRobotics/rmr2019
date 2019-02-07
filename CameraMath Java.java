f134cc202cdb730e4d5a41a749b58e3f18e13aa3
198405 <46546887+198405@users.noreply.github.com>
2019-02-06 19:12:25 -0700

    Update CameraMath Java.java

--- CameraMath Java.java
+++ CameraMath Java.java
@@ -1,59 +1,65 @@
-//This code was written by Nathan Jensen
-//to calculate the angles for the camera data
+package frc.robot;
 
-//This code is mostly untested.
+import java.lang.Math;
 
-import math.sqrt;
-import math.arctan;
-
-public class CameraMath_Java{
-    public static void main(String[] args){
-        CameraMath math = new CameraMath(8, 10); //the first number is the height of the camera in inches and the second number is the error bars for the calculations. Higher = more relaxed standards.
-        System.out.println(math.math([/*replace this with test points*/]));
+public class CameraMath{
+    public CameraMath(int i, int j) {
     }
+
+    public static void main(String[] args) {
+        CameraMath1 Camera = new CameraMath1(8, 10); //the first number is the height of the camera in inches and the second number is the error bars for the calculations. Higher = more relaxed standards.
+        int points2 [] = {-88, -619, -275, -619, -275, -750, -88, -750};
+    System.out.println(Camera.math(points2));
+    }
+
+    /*private char[] math(int[] points2) {
+        return null;
+    }*/
 }
 
-public class CameraMath extends Component{
+class CameraMath1 // extends Component
+{
+
     //the amount of pixels in 1 inch
-    int inch;
+    double inch;
     //the amount of pixels dists can be off
-    int error;
+    double error;
 
     //constructor - one arg - height of camera off the ground
-    public CameraMath(double height, int error){
+    public CameraMath1(double in_height, int in_error){
         //this sets inch in to the correct number
-        inch = 2000 / (height * (5.039 / 6));
+        inch = 2000 / (in_height * (5.039 / 6));
 
         //this sets the error to the right error
-        this.error = error;
+        error = in_error;
     }
 
     //this is the main math function
-    public math(int[8] points){
+    public double math(int[] points){
         //find the highest point
-        highest_index = 1;
+        int highest_index = 1;
         for(int i = 1; i < 8; i += 1){
             if(points[i] > points[highest_index]){
                 highest_index = i;
             }
         }
-        index--;
+        highest_index--;
 
         //get the distances from that point
-        int xa = (points[(highest_index - 2) % 4] - points[(highest_index)]);
-        int ya = (points[(highest_index - 1) % 4] - points[(highest_index + 1)]);
-        int dist_a = sqrt(xa * xa + ya * ya);
-        int xb = (points[(highest_index + 2) % 4] - points[(highest_index)]);
-        int yb = (points[(highest_index + 3) % 4] - points[(highest_index) + 1]);
-        int dist_b = sqrt(xb * xb + yb * yb);
+        double xa = (points[(highest_index - 2) % 4] - points[(highest_index)]);
+        double ya = (points[(highest_index - 1) % 4] - points[(highest_index + 1)]);
+        double dist_a = Math.sqrt(xa * xa + ya * ya);
+        double xb = (points[(highest_index + 2) % 4] - points[(highest_index)]);
+        double yb = (points[(highest_index + 3) % 4] - points[(highest_index) + 1]);
+        double dist_b = Math.sqrt(xb * xb + yb * yb);
 
         //figure out which direction it will go
         if(dist_a == dist_b){
             return 0;
         } else if(2 * inch - error < dist_a && dist_a < 2 * inch + error){
-            return arctan(ya / xa);
+            return Math.atan(ya / xa);
         } else if(2 * inch - error < dist_b && dist_b < 2 * inch + error){
-            return arctan(yb / xb);
+            return Math.atan(yb / xb);
         } else {
             //throw an exception if the input is bad
             throw new IllegalArgumentException("Error in CameraMath Java.java: You wrote the code above this error thrower badly. Try fixing it until the code stops erring.");
@@ -63,8 +69,8 @@ public class CameraMath extends Component{
     }
 
     //this is for tick updates
-    @Override
+    /*@Override
     public Update(){
 
-    }
+    }*/
 }
