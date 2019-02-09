2434e6b46629d4881962402d2b7352e4563efd4e
CrypticCole <46848316+CrypticCole@users.noreply.github.com>
2019-02-09 11:27:46 -0700

    Create TapeMathSlope.java
    This version of tape math finds the slope of a line running down the middle of the rectangle. It turns the robot an amount inversely until the slope no longer exist or in other words the line is vertical and lined up. The while loop condition can be changed to a dead zone instead of exactly zero.

---
+++ TapeMathSlope.java
@@ -0,0 +1,169 @@
+
+
+public class TapeMathSlope
+{
+	int x1 = 9999;
+
+	int y1 = 9999;
+
+	int pos1 = 0;
+
+
+	int x2 = -9999;
+
+	int y2 = 9999;
+
+	int pos2 = 0;
+
+
+	int x3 = -9999;
+
+	int y3 = 0;
+
+	int pos3 = 0;
+	int x4 = 0;
+	int y4 = 0;
+
+	double line12;
+	double line14;
+
+	double xTopAvg;
+	double xBottomAvg;
+	double yTopAvg;
+	double yBottomAvg;
+	double slope;
+
+	//Placeholder turn values
+	double turnValue;
+	double turnConstant=1;
+
+
+	//Find the first point
+
+	for(int i = 0; i<7; i+=2){
+		if((array[i+1] < y1) || (array[i+1] == y1 && array[i] < x1)){
+	
+			x1 = array[i];
+				
+			y1 = array[i+1];
+					
+			pos1 = i;
+		
+		}
+			
+	}
+
+			
+
+	//Find the second point
+			
+	for(int i = 0; i<7;i+=2){
+			
+		if(i!=pos1 && (array[i] > x2 || (array[i] == x2 && array[i+1] < y2))){
+		
+			x2 = array[i];
+		
+			y2 = array[i+1];
+			
+			pos2 = i;
+			
+		}
+		
+	}
+
+			
+
+	//Find the third point
+	
+	for(int i = 0; i < 7; i+=2){
+		
+		if((i!=pos1 && i!=pos2) && array[i] > x3){
+
+			x3 = array[i];
+		
+			y3 = array[i+1];
+			
+			pos3 = i;
+				
+		}
+			
+	}
+
+			
+
+	//Find the fourth point
+			
+	for(int i = 0; i < 7; i+=2){
+		
+		if(i!=pos1 && i!=pos2 && i!=pos3){
+			
+			x4=array[i];
+				
+			y4=array[i+1];
+		
+		}
+		
+	}
+
+	//Calculating inital avgeraged points
+	if(y4<y2) //If true y4 and y1 are top points if false y1 and y2 are top points
+	{
+		xTopAvg=(x1+x4)/2;
+		yTopAvg=(y1+y4)/2;
+		xBottomAvg=(x3+x2)/2;
+		yBottomAvg=(y3+y2)/2;
+		turnConstant=turnConstant*-1;
+	}
+	else
+	{
+		xTopAvg=(x1+x2)/2;
+		yTopAvg=(y1+y2)/2;
+		xBottomAvg=(x3+x4)/2;
+		yBottomAvg=(y3+y4)/2;
+	}
+
+
+	//Loop to turn until correct
+	while(xtopAvg-xBottomAvg!=0)
+	{
+		//Get slope
+		slope=(yTopAvg-yBottomAvg)/(xTopAvg-xBottomAvg);
+
+		//Test Output
+		System.out.print(slope);
+
+		//The robot will turn an amount inversly proportional to the slope.
+		if(slope!=0)
+		{
+			turnValue=turnConstant/slope;
+		}
+		else
+		{
+			//Plan if rectangle is perfecly horizontal
+		}
+
+
+		//Call functions to turn robot and functions to get new camera data here
+
+
+		//Updating the avg points
+		if(y4<y2) //If true y4 and y1 are top points if false y1 and y2 are top points
+		{
+			xTopAvg=(x1+x4)/2;
+			yTopAvg=(y1+y4)/2;
+			xBottomAvg=(x3+x2)/2;
+			yBottomAvg=(y3+y2)/2;
+			turnConstant=turnConstant*-1;
+		}
+		else
+		{
+			xTopAvg=(x1+x2)/2;
+			yTopAvg=(y1+y2)/2;
+			xBottomAvg=(x3+x4)/2;
+			yBottomAvg=(y3+y4)/2;
+		}
+	}
+}		
+
+
+
